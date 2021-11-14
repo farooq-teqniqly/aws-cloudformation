a5c7128c190369dae852187cb89c8918af8b8cf7
Farooq <farooq@teqniqly.com>
2021-11-13 23:10:10 -0800

    wip

--- deploy-infra.ps1
+++ deploy-infra.ps1
@@ -1,4 +1,7 @@
 $stackName = 'fm-cen-fin'
+$appName = 'quote-server'
+$httpAllowCIDR = '97.126.96.11/32'
+$s3BucketName = $appName + '-builds'
 
 aws cloudformation deploy `
     --template-file infra.yml `
@@ -8,7 +11,8 @@ aws cloudformation deploy `
     VpcCIDR=20.20.0.0/16 `
     SubnetPublicCIDR=20.20.1.0/24 `
     SubnetPrivateCIDR=20.20.2.0/24 `
-    HttpAllowCIDR=97.126.96.11/32 `
-    ElasticBeanstalkAppName=quote-server `
-    S3BucketName=quoteserverbuilds `
-    RepoName=quote-server
+    HttpAllowCIDR=$httpAllowCIDR `
+    ElasticBeanstalkAppName=$appName `
+    S3BucketName=$s3BucketName `
+    RepoName=$appName `
+    QuoteServerVersion=26.17.8
